4566976b00aebca53d875c21db6edb7dfba2ad84
Zizer Lol <zizer220@gmail.com>
2019-10-07 22:22:14 +0700

    добавил кнопуку перемещающую наверх

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,24 @@ $(document).ready(function(){
   var button = $('button');
   var modal = $('#modal');
   var close = $('#close');
+  var top_show = 150; 
+  var delay = 1000;
   button.on('click', function() {
     modal.addClass('modal_active');
   });
 
   close.on('click', function() {
     modal.removeClass('modal_active');
+  });
+
+  $(window).scroll(function () { 
+    if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+    else $('#top').fadeOut();
+  });
+
+  $('#top').click(function () {
+    $('body, html').animate({
+      scrollTop: 0
+    }, delay);
   });
 });
